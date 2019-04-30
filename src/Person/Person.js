9bf25e426ed41ebe20b0f15d2f149a50e4da145a
Rachel Akisada <raakisad@microsoft.com>
2019-04-29 18:34:51 -0700

    Added dynamic age to Person const.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
+//In order to get Jsx to execute javascript rather than printing string wrap things in {}
 const person = () => {
-    return <p>I'm a Person!</p>
+    return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
 };
 
 export default person;
